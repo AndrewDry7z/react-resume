afaf6d372a265e52df786a8a91f156e7acb74679
Andrew Chufistov <andrewch136@yandex.ru>
2020-07-11 22:16:14 +0300

    New projects

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -45,25 +45,26 @@ function Projects() {
                     <p className="projects-item__name">Sales Generator LP</p>
                     <a href="//sales-generator.ru/company" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'sg-company'} alt={'SEO Agency LP'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
-                        <a href="//sales-generator.ru/company" rel={'nofollow'} target={'_blank'}>sales-generator.ru/company</a>
+                        <a href="//sales-generator.ru/company" rel={'nofollow'}
+                           target={'_blank'}>sales-generator.ru/company</a>
                     </p>
                     <p className="projects-item__description">
-                       SEO and Internet Marketing agency landing page
+                        SEO and Internet Marketing agency promo landing page
                     </p>
                     <p className="projects-item__tech">
-                        <Picture classname={''} name={'sass_icon'} type={'png'} alt={'SCSS'}/>
-                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'}/>
-                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JS'}/>
+                        <Picture classname={''} name={'sass_icon'} type={'png'} alt={'SCSS'} />
+                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'} />
+                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JS'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Dynamic Photo Gallery</p>
                     <a href="/projects/mywed-demo/" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'mywed-demo'} alt={'Photo Gallery'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="/projects/mywed-demo/" rel={'nofollow'} target={'_blank'}>mywed-demo</a>
@@ -72,14 +73,14 @@ function Projects() {
                         Dynamic photo gallery on Javascript ES6
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JS'}/>
+                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JS'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Legint</p>
                     <a href="//en.legint.com" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'legint.com-small'} alt={'Legint'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//en.legint.com" rel={'nofollow'} target={'_blank'}>en.legint.com</a>
@@ -88,14 +89,14 @@ function Projects() {
                         Multilingual Wordpress website for political technology bureau focused on the Middle East
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'wordpress_icon'} type={'png'} alt={'Wordpress'}/>
-                        <Picture classname={''} name={'vc_icon'} type={'png'} alt={'Visual Composer'}/>
+                        <Picture className={''} name={'wordpress_icon'} type={'png'} alt={'Wordpress'} />
+                        <Picture classname={''} name={'vc_icon'} type={'png'} alt={'Visual Composer'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Repair Calc</p>
                     <a href="//myremontnow.ru/calculator/" rel={'nofollow'} target={'_blank'}>
-                        <Picture className={'projects-item__image'} name={'myremont'} alt={'MyRemont'} type={'jpg'}/>
+                        <Picture className={'projects-item__image'} name={'myremont'} alt={'MyRemont'} type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//myremontnow.ru/calculator/" rel={'nofollow'}
@@ -105,16 +106,16 @@ function Projects() {
                         Repair cost and terms calculator LP for construction/repair company based in Moscow
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'}/>
-                        <Picture classname={''} name={'js_icon'} type={'png'} alt={'JavaScript'}/>
-                        <Picture classname={''} name={'ajax_icon'} type={'png'} alt={'AJAX'}/>
+                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'} />
+                        <Picture classname={''} name={'js_icon'} type={'png'} alt={'JavaScript'} />
+                        <Picture classname={''} name={'ajax_icon'} type={'png'} alt={'AJAX'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Sales Generator</p>
                     <a href="//sales-generator.ru" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'sg'} alt={'Sales Generator'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//sales-generator.ru" rel={'nofollow'} target={'_blank'}>sales-generator.ru</a>
@@ -123,17 +124,17 @@ function Projects() {
                         Bitrix-based website for SEO and Internet Marketing agency
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'bitrix_icon'} type={'png'} alt={'Bitrix'}/>
-                        <Picture className={''} name={'php_icon'} type={'png'} alt={'PHP'}/>
-                        <Picture classname={''} name={'js_icon'} type={'png'} alt={'JavaScript'}/>
-                        <Picture classname={''} name={'sass_icon'} type={'png'} alt={'SCSS'}/>
+                        <Picture className={''} name={'bitrix_icon'} type={'png'} alt={'Bitrix'} />
+                        <Picture className={''} name={'php_icon'} type={'png'} alt={'PHP'} />
+                        <Picture classname={''} name={'js_icon'} type={'png'} alt={'JavaScript'} />
+                        <Picture classname={''} name={'sass_icon'} type={'png'} alt={'SCSS'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Personal Website</p>
                     <a href="//achufistov.ru" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'kolbasim'} alt={'Personal Website'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//achufistov.ru" rel={'nofollow'} target={'_blank'}>achufistov.ru</a>
@@ -142,15 +143,31 @@ function Projects() {
                         You're here now :)
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'reactjs_icon'} type={'png'} alt={'ReactJS'}/>
-                        <Picture className={''} name={'sass_icon'} type={'png'} alt={'SCSS'}/>
+                        <Picture className={''} name={'reactjs_icon'} type={'png'} alt={'ReactJS'} />
+                    </p>
+                </div>
+                <div className="projects-item">
+                    <p className="projects-item__name">Creative Soldiers Special Project</p>
+                    <a href="//creativesoldiers.ru/special-project/" rel={'nofollow noopener'} target={'_blank'}>
+                        <Picture className={'projects-item__image'} name={'cssp'} alt={'Personal Website'}
+                                 type={'jpg'} />
+                    </a>
+                    <p className="projects-item__link">
+                        <a href="//creativesoldiers.ru/special-project/" rel={'nofollow'}
+                           target={'_blank'}>creativesoldiers.ru/special-project/</a>
+                    </p>
+                    <p className="projects-item__description">
+                        Promo landing page for Creative Soldiers
+                    </p>
+                    <p className="projects-item__tech">
+                        <Picture className={''} name={'vue_icon'} type={'png'} alt={'VueJS'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Car service center LP</p>
                     <a href="//nakolesah.site/diski/" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'nakolesah'} alt={'Car service center LP'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//nakolesah.site/diski/" rel={'nofollow'} target={'_blank'}>nakolesah.site/diski/</a>
@@ -159,15 +176,33 @@ function Projects() {
                         Landing page for car service center based in Moscow
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'}/>
-                        <Picture className={''} name={'bootstrap_icon'} type={'png'} alt={'Bootstrap'}/>
+                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'} />
+                        <Picture className={''} name={'bootstrap_icon'} type={'png'} alt={'Bootstrap'} />
+                    </p>
+                </div>
+                <div className="projects-item">
+                    <p className="projects-item__name">SCAN</p>
+                    <a href="//scan-interfax.ru" rel={'nofollow noopener'} target={'_blank'}>
+                        <Picture className={'projects-item__image'} name={'scan'} alt={'SCAN'}
+                                 type={'jpg'} />
+                    </a>
+                    <p className="projects-item__link">
+                        <a href="//scan-interfax.ru" rel={'nofollow noopener'} target={'_blank'}>scan-interfax.ru</a>
+                    </p>
+                    <p className="projects-item__description">
+                        New website of news agency product
+                    </p>
+                    <p className="projects-item__tech">
+                        <Picture className={''} name={'wordpress_icon'} type={'png'} alt={'WP'} />
+                        <Picture className={''} name={'sass_icon'} type={'png'} alt={'SCSS'} />
+                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JS'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Nail files shop</p>
                     <a href="//mirpilok.ru/" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'mirpilok'} alt={'MirPilok Shop'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="//mirpilok.ru/" rel={'nofollow'} target={'_blank'}>mirpilok.ru</a>
@@ -176,16 +211,15 @@ function Projects() {
                         Wordpress (WooCommerce) online shop with own blog
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'wordpress_icon'} type={'png'} alt={'Wordpress'}/>
-                        <Picture className={''} name={'sass_icon'} type={'png'} alt={'SCSS'}/>
-                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JavaScript'}/>
+                        <Picture className={''} name={'wordpress_icon'} type={'png'} alt={'Wordpress'} />
+                        <Picture className={''} name={'js_icon'} type={'png'} alt={'JavaScript'} />
                     </p>
                 </div>
                 <div className="projects-item">
                     <p className="projects-item__name">Interactive US map</p>
                     <a href="/projects/us-map/" rel={'nofollow'} target={'_blank'}>
                         <Picture className={'projects-item__image'} name={'usmap'} alt={'US interactive map'}
-                                 type={'jpg'}/>
+                                 type={'jpg'} />
                     </a>
                     <p className="projects-item__link">
                         <a href="/projects/us-map/" rel={'nofollow'} target={'_blank'}>us-map</a>
@@ -194,8 +228,8 @@ function Projects() {
                         Visual presentation of the educational project for RUDN University
                     </p>
                     <p className="projects-item__tech">
-                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'}/>
-                        <Picture className={''} name={'svg_icon'} type={'png'} alt={'SVG'}/>
+                        <Picture className={''} name={'html5_icon'} type={'png'} alt={'HTML5'} />
+                        <Picture className={''} name={'svg_icon'} type={'png'} alt={'SVG'} />
                     </p>
                 </div>
             </TinySlider>
